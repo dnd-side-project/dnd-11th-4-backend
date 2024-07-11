a92a9fd8bce6a31cf59754f182b9b6db49742cf4
youngreal <59333182+youngreal@users.noreply.github.com>
2024-07-11 19:44:45 +0900

    init

---
+++ src/main/java/com/dnd/dndtravel/DndTravelProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.dndtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndTravelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndTravelProjectApplication.class, args);
+	}
+
+}
