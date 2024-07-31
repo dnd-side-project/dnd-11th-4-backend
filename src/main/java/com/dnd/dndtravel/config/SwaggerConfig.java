38cf2b6b4d7c92a2a5128086c94aa8c5a13e51ee
min-0 <dhe08133@naver.com>
2024-07-31 23:44:04 +0900

    ✨ Swagger Setting
    API docs 관련 의존성주입, config 파일 작성

---
+++ src/main/java/com/dnd/dndtravel/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.dnd.dndtravel.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MAPDDANG API")
+                        .description("맵땅 앱 관련 API")
+                        .version("1.0.0"));
+
+    }
+}
