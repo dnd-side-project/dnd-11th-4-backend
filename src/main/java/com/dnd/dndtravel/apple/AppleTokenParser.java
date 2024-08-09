88cad0ccac62bc715739ceee10c28158b798878f
min-0 <dhe08133@naver.com>
2024-08-09 23:51:11 +0900

    ✨ Apple 로그인 구현
    애플 서버에서 받아 온 jwk 검증, 정보 추출 구현

---
+++ src/main/java/com/dnd/dndtravel/apple/AppleTokenParser.java
@@ -0,0 +1,54 @@
+package com.dnd.dndtravel.apple;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.PublicKey;
+import java.util.Map;
+
+/*
+JWK 리스트 조회
+헤더 부분을 디코딩하여 일치하는 jwk를 찾을 alg, kid 값을 얻고, id_token Claim 추출
+ */
+@RequiredArgsConstructor
+@Component
+public class AppleTokenParser {
+    private static final String IDENTITY_TOKEN_VALUE_DELIMITER = "\\.";
+    private static final int HEADER_INDEX = 0;
+
+    private final ObjectMapper objectMapper;
+
+    public Map<String, String> parseHeader(final String appleToken) {
+        try {
+            final String decodedHeader = appleToken.split(IDENTITY_TOKEN_VALUE_DELIMITER)[HEADER_INDEX];
+            return objectMapper.readValue(decodedHeader, Map.class);
+        } catch (JsonMappingException e) {
+            throw new RuntimeException("appleToken 값이 jwt 형식인지, 값이 정상적인지 확인해주세요.");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("디코드된 헤더를 Map 형태로 분류할 수 없습니다. 헤더를 확인해주세요.");
+        }
+    }
+
+    public Claims extractClaims(final String appleToken, final PublicKey publicKey) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(publicKey)
+                    .build()
+                    .parseSignedClaims(appleToken)
+                    .getPayload();
+        } catch (UnsupportedJwtException e) {
+            throw new UnsupportedJwtException("지원되지 않는 jwt 타입");
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("비어있는 jwt");
+        } catch (JwtException e) {
+            throw new JwtException("jwt 검증 or 분석 오류");
+        }
+    }
+}
---
+++ src/main/java/com/dnd/dndtravel/apple/dto/AppleUser.java
@@ -0,0 +1,14 @@
+package com.dnd.dndtravel.apple.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AppleUser {
+    private final String name;
+    private final String email;
+
+    public AppleUser(final String name, final String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
