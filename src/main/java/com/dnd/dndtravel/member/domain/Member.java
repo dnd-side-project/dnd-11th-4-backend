797db293e411d7f6ecd4be450353170debed5e29
min-0 <dhe08133@naver.com>
2024-08-14 17:17:31 +0900

    :sparkles: member entity 생성

---
+++ src/main/java/com/dnd/dndtravel/member/domain/Member.java
@@ -0,0 +1,35 @@
+package com.dnd.dndtravel.member.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    public static Member from(String name, String email){
+        return Member.builder()
+                .name(name)
+                .email(email)
+                .build();
+    }
+}
