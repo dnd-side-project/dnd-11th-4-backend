c8e51f59f34eec31890dfd87ccb61926ee97b345
min-0 <dhe08133@naver.com>
2024-08-19 03:44:38 +0900

    ✨ Create Access Token 메소드 생성
    MemberId가 담긴 Access Token 생성, JWT_SECRET_KEY 추가

--- src/main/java/com/dnd/dndtravel/auth/service/AuthTokenService.java
+++ src/main/java/com/dnd/dndtravel/auth/service/AuthTokenService.java
@@ -1,45 +1,51 @@
 package com.dnd.dndtravel.auth.service;
 
 import com.dnd.dndtravel.auth.domain.AuthToken;
-import com.dnd.dndtravel.auth.dto.AuthMember;
 import com.dnd.dndtravel.auth.repository.AuthTokenRepository;
 import com.dnd.dndtravel.member.domain.Member;
-import com.dnd.dndtravel.member.service.MemberService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class AuthTokenService {
+
     private final AuthTokenRepository authTokenRepository;
-
-    private final MemberService memberService;
-
-    public static ConcurrentHashMap<String, AuthMember> accessTokenMap = new ConcurrentHashMap(256);
+    private static final String secretKey = "${JWT_SECRET_KEY}";
 
     @Transactional
     public AuthToken issue(final Member member) {
         final Optional<AuthToken> byMemberId = authTokenRepository.findByMemberId(member.getId());
+        return getInnerAuthToken(member, byMemberId);
+    }
+
+    private AuthToken getInnerAuthToken(Member member, Optional<AuthToken> byMemberId) {
         AuthToken authToken;
+
         if (byMemberId.isEmpty()) {
+            String accessToken = createAccessToken(member.getId());  // Access Token 생성 시 MemberId 포함
             authToken = authTokenRepository.save(AuthToken.of(member.getId()));
         } else {
             authToken = byMemberId.get();
-            final String accessToken = authToken.getAccessToken();
-            accessTokenMap.remove(accessToken);
             authToken.reIssuance();
         }
-        log.info("memberId: " + member.getId());
-        log.info("access 토근 발급: " + authToken.getAccessToken());
-        log.info("refresh 토근 발급: " + authToken.getRefreshToken());
-        final AuthMember authMember = new AuthMember(member);
-        accessTokenMap.put(authToken.getAccessToken(), authMember);
         return authToken;
+    }
+
+    private String createAccessToken(Long memberId) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .subject(String.valueOf(memberId))
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + 1000 * 60 * 60 * 24)) //만료 기간 1일
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
     }
 }
