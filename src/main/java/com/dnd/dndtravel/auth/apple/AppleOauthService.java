73ea05be1a307c0e2685c5069e59accadc77a7c9
min-0 <dhe08133@naver.com>
2024-08-19 16:57:57 +0900

    :recycle: 상수를 static으로 변경 후 코드 위치 변경

--- src/main/java/com/dnd/dndtravel/auth/apple/AppleOauthService.java
+++ src/main/java/com/dnd/dndtravel/auth/apple/AppleOauthService.java
@@ -12,12 +12,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Component
 public class AppleOauthService {
+    private static final String DEFAULT_NAME = "apple";
+    private static final String CLAIM_EMAIL = "email";
     private final AppleTokenParser appleTokenParser;
     private final AppleClient appleClient;
     private final ApplePublicKeyGenerator applePublicKeyGenerator;
-
-    private final String DEFAULT_NAME = "apple";
-    private final String CLAIM_EMAIL = "email";
 
     public AppleUser createAppleUser(final String appleToken) {
         final Map<String, String> appleTokenHeader = appleTokenParser.parseHeader(appleToken);
