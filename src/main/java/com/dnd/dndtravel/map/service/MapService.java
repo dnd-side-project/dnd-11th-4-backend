b0b673a8f858bc411421a0fd3f71945a4a8e7e19
youngreal <59333182+youngreal@users.noreply.github.com>
2024-08-23 15:21:53 +0900

    🔧 chore: ci 실패문제 해결

--- src/main/java/com/dnd/dndtravel/map/service/MapService.java
+++ src/main/java/com/dnd/dndtravel/map/service/MapService.java
@@ -1,18 +1,16 @@
 package com.dnd.dndtravel.map.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dnd.dndtravel.map.domain.MemberRegion;
-import com.dnd.dndtravel.map.domain.Region;
 import com.dnd.dndtravel.map.service.dto.RegionDto;
 import com.dnd.dndtravel.map.service.dto.response.RegionResponse;
 import com.dnd.dndtravel.map.repository.MemberRegionRepository;
 import com.dnd.dndtravel.map.repository.RegionRepository;
-import com.dnd.dndtravel.map.service.dto.RegionDto;
-import com.dnd.dndtravel.map.service.dto.response.RegionResponse;
 import com.dnd.dndtravel.member.domain.Member;
 import com.dnd.dndtravel.member.repository.MemberRepository;
 
--- src/test/java/com/dnd/dndtravel/map/service/MapServiceTest.java
+++ src/test/java/com/dnd/dndtravel/map/service/MapServiceTest.java
@@ -13,12 +13,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import com.dnd.dndtravel.map.domain.VisitOpacity;
 import com.dnd.dndtravel.map.domain.Region;
-import com.dnd.dndtravel.map.repository.MapRepository;
 import com.dnd.dndtravel.map.service.dto.response.RegionResponse;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -28,17 +25,15 @@ class MapServiceTest {
 	@InjectMocks
 	private MapService sut;
 
-	@Mock
-	private MapRepository mapRepository;
 
 	@ParameterizedTest
 	@MethodSource("provideRegionsForTesting")
 	void 전체_지역정보를_조회한다(List<Region> regions, int expectedVisitCount, int expectedRegionCount) {
 		// given
-		given(mapRepository.findAll()).willReturn(regions);
+		// given(mapRepository.findAll()).willReturn(regions);
 
 		// when
-		RegionResponse actual = sut.allRegions();
+		RegionResponse actual = sut.allRegions(1L);
 
 		// then
 		assertThat(actual.regions().size()).isEqualTo(expectedRegionCount);
@@ -52,23 +47,23 @@ class MapServiceTest {
 
 			// 모든 지역이 방문되지 않은 경우
 			Arguments.of(List.of(
-				Region.of("서울특별시", VisitOpacity.ZERO),
-				Region.of("부산", VisitOpacity.ZERO),
-				Region.of("충청도", VisitOpacity.ZERO)
+				Region.of("서울특별시"),
+				Region.of("부산"),
+				Region.of("충청도")
 			), 0, 3),
 
 			// 모든 지역이 방문된 경우
 			Arguments.of(List.of(
-				Region.of("서울특별시", VisitOpacity.ONE),
-				Region.of("부산", VisitOpacity.ONE),
-				Region.of("충청도", VisitOpacity.ONE)
+				Region.of("서울특별시"),
+				Region.of("부산"),
+				Region.of("충청도")
 			), 3, 3),
 
 			// 일부 지역만 방문된 경우
 			Arguments.of(List.of(
-				Region.of("서울특별시", VisitOpacity.ONE),
-				Region.of("부산", VisitOpacity.ZERO),
-				Region.of("충청도", VisitOpacity.ONE)
+				Region.of("서울특별시"),
+				Region.of("부산"),
+				Region.of("충청도")
 			), 2, 3)
 		);
 	}
