0eb8d4167989d8cd2460605dfc77d867ba95b93a
youngreal <59333182+youngreal@users.noreply.github.com>
2024-08-27 11:09:26 +0900

    🔥 불필요 코드 삭제

--- src/main/java/com/dnd/dndtravel/map/controller/request/validation/RegionEnum.java
+++ src/main/java/com/dnd/dndtravel/map/controller/request/validation/RegionEnum.java
@@ -6,14 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
 
 @Constraint(validatedBy = {RegionValidator.class})
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RegionEnum {
 	String message() default "invalid region";
-	Class<?>[] groups() default {};
-	Class<? extends Payload>[] payload() default {};
 	Class<? extends java.lang.Enum<?>> enumClass();
 }
--- src/main/java/com/dnd/dndtravel/map/dto/RecordDto.java
+++ src/main/java/com/dnd/dndtravel/map/dto/RecordDto.java
@@ -6,9 +6,7 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Builder;
-/**
- *
- */
+
 @Builder
 public record RecordDto(
 	String region,
