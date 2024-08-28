16b8fb62f279563c8af5ac121e68e6477e47fd22
youngreal <59333182+youngreal@users.noreply.github.com>
2024-08-28 22:10:50 +0900

    🎨 test disabled 처리
    * 현재 빌드 배포시 테스트 실행하지않고, 당장 필요하지않기에 disable

--- src/test/java/com/dnd/dndtravel/map/service/MapServiceTest.java
+++ src/test/java/com/dnd/dndtravel/map/service/MapServiceTest.java
@@ -1,8 +1,15 @@
 package com.dnd.dndtravel.map.service;
 
 import com.dnd.dndtravel.map.domain.Region;
-import com.dnd.dndtravel.map.domain.VisitOpacity;
 import com.dnd.dndtravel.map.service.dto.response.RegionResponse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,16 +17,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.List;
-import java.util.stream.Stream;
+import org.mockito.Mock;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
+import org.mockito.junit.jupiter.MockitoExtension;
 import com.dnd.dndtravel.map.domain.MemberRegion;
 import com.dnd.dndtravel.map.repository.MemberRegionRepository;
 import com.dnd.dndtravel.map.repository.RegionRepository;
+import com.dnd.dndtravel.member.domain.Member;
+import com.dnd.dndtravel.member.repository.MemberRepository;
 
+@Disabled("테스트 코드는 추후 사용할 예정")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @ExtendWith(MockitoExtension.class)
 class MapServiceTest {
@@ -41,6 +48,14 @@ class MapServiceTest {
 	void 전체_지역정보를_조회한다(List<Region> regions, int expectedVisitCount, int expectedRegionCount) {
 		// given
 		// given(mapRepository.findAll()).willReturn(regions);
+		String a= "충남·세종";
+		Member member = Member.of("유저1", "user12345@naver.com", "RED");
+		Region region = Region.of("서울");
+		given(memberRepository.findById(1L)).willReturn(Optional.of(member));
+		given(memberRegionRepository.findByMemberId(1L)).willReturn(
+			List.of(MemberRegion.of(member, region))
+		);
+		given(regionRepository.findAll()).willReturn(regions);
 
 		// when
 		RegionResponse actual = sut.allRegions(1L);
