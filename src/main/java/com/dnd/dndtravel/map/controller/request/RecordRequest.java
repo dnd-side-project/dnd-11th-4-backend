2479c0bcf5b0be2420c940c6841373f6ee4247e6
youngreal <59333182+youngreal@users.noreply.github.com>
2024-09-04 02:41:57 +0900

    ✨#31 - swaager 적용
    * 컨트롤러 코드에서라도 최소한 Swagger 관련 어노테이션들을 걷어내기 인터페이스 사용
    
    * JWT 토큰을 보내야하는 API에 공통으로 사용되는 스웨거 어노테이션들을 @AuthenticationCommonResponse로 응집

--- src/main/java/com/dnd/dndtravel/map/controller/request/RecordRequest.java
+++ src/main/java/com/dnd/dndtravel/map/controller/request/RecordRequest.java
@@ -1,5 +1,8 @@
 package com.dnd.dndtravel.map.controller.request;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -9,23 +12,25 @@ import com.dnd.dndtravel.map.controller.request.validation.RegionCondition;
 import com.dnd.dndtravel.map.controller.request.validation.RegionEnum;
 import com.dnd.dndtravel.map.dto.RecordDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-//todo 회의 후 제약조건 변경 필요
 public record RecordRequest(
+	@Schema(description = "지역 이름", requiredMode = REQUIRED)
 	@RegionEnum(enumClass = RegionCondition.class)
 	String region,
 
-	@NotBlank(message = "명소 이름은 필수 입력 사항입니다.")
-	@Pattern(regexp = "^[가-힣]+$", message = "명소 이름은 한글만 입력 가능합니다.")
-	@Size(max = 50, message = "명소 이름은 50자 이내여야 합니다.")
+	@Schema(description = "명소명", requiredMode = REQUIRED)
+	@NotBlank(message = "명소명은 필수 입니다.")
+	@Size(max = 10, message = "명소 이름은 10자 이내여야 합니다.")
 	String attractionName,
 
+	@Schema(description = "메모", requiredMode = NOT_REQUIRED)
 	@Size(max = 25, message = "메모는 25자 이내여야 합니다.")
 	String memo,
 
+	@Schema(description = "방문날짜, ISO Date(yyyy-MM-dd) 형식으로 입력", requiredMode = NOT_REQUIRED)
 	@NotNull(message = "날짜는 필수 입력 사항입니다.")
 	LocalDate localDate
 ) {
