3fed7ad8ebfa689422c1993936bdadfa5c5a8eea
youngreal <59333182+youngreal@users.noreply.github.com>
2024-11-01 17:17:31 +0900

    ♻️ PhotoValidation 누락된 메서드들 추가

--- src/main/java/com/dnd/dndtravel/map/controller/request/validation/PhotoValidation.java
+++ src/main/java/com/dnd/dndtravel/map/controller/request/validation/PhotoValidation.java
@@ -6,10 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 
 @Constraint(validatedBy = {PhotoValidator.class})
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PhotoValidation {
 	String message() default "invalid photo size";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
 }
